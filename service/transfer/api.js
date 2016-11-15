11d7d15f7b052193c5f2b356f932c1ce56bbdd23
stamen.peev <stamen.peev@softwaregroup-bg.com>
2016-11-15 16:33:22 +0200

    fix: validations

--- service/transfer/api.js
+++ service/transfer/api.js
@@ -6,15 +6,26 @@ module.exports = {
     // tags: ['tag1', 'tag2'],
     description: 'Transfer push execute',
     notes: 'Transfer push execute',
+    auth: false,
     params: joi.object({
       currency: joi.string().required(),
       destinationAccount: joi.string().required(),
       destinationAmount: joi.string().required(),
       fee: joi.number().required(),
       sourceAccount: joi.string().required()
-    }),
+    }).unknown(),
     result: joi.object({
       fulfillment: joi.string().required()
-    })
+    }).unknown()
+  },
+  'rule.fetch': {
+    // description: 'some description of the method',
+    // notes: ['some notes about the method'],
+    // tags: ['tag1', 'tag2'],
+    auth: false,
+    description: 'Transfer rule fetch',
+    notes: 'Transfer rule fetch',
+    params: joi.any(),
+    result: joi.any()
   }
 }
