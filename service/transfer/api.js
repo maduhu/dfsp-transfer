3581dc9613cf7e587949afc61bb99053f9e7b8c5
stamen.peev <stamen.peev@softwaregroup-bg.com>
2016-12-08 17:43:51 +0200

    fix: temporarily set more loose validations

--- service/transfer/api.js
+++ service/transfer/api.js
@@ -7,16 +7,8 @@ module.exports = {
     description: 'Transfer push execute',
     notes: 'Transfer push execute',
     auth: false,
-    params: joi.object({
-      currency: joi.string().required(),
-      destinationAccount: joi.string().required(),
-      destinationAmount: joi.string().required(),
-      fee: joi.number().required(),
-      sourceAccount: joi.string().required()
-    }).unknown(),
-    result: joi.object({
-      fulfillment: joi.string().required()
-    }).unknown()
+    params: joi.any(),
+    result: joi.any()
   },
   'invoice.add': {
     auth: false,
@@ -29,39 +21,28 @@ module.exports = {
     auth: false,
     description: 'Get invoice',
     notes: 'Get invoice',
-    params: joi.object().keys({
-      invoiceId: joi.string().required()
-    }),
+    params: joi.any(),
     result: joi.any()
   },
   'invoice.edit': {
     auth: false,
     description: 'Edit invoice',
     notes: 'Edit invoice',
-    params: joi.object().keys({
-      invoiceId: joi.string().required(),
-      statusCode: joi.string().required()
-    }),
+    params: joi.any(),
     result: joi.any()
   },
   'invoiceNotification.add': {
     auth: false,
     description: 'Transfer add invoice',
     notes: 'Transfer add invoice',
-    params: joi.object().keys({
-      invoiceUrl: joi.string().required(),
-      senderIdentifier: joi.string(),
-      memo: joi.string().required()
-    }).unknown(),
+    params: joi.any(),
     result: joi.any()
   },
   'invoiceNotification.fetch': {
     auth: false,
     description: 'Transfer invoiceNotification fetch',
     notes: 'TraTransfer invoiceNotification fetch',
-    params: joi.object().keys({
-      userNumber: joi.string().required()
-    }),
+    params: joi.any(),
     result: joi.any()
   }
 }
