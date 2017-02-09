dc79b0f58b8f07c1dc973420c4fafdc2f521beac
stamen.peev <stamen.peev@softwaregroup-bg.com>
2017-02-09 16:12:40 +0200

    fix: add httpserver validations skeleton

--- service/bulk/api.js
+++ service/bulk/api.js
@@ -6,5 +6,40 @@ module.exports = {
     notes: 'Bulk batch add',
     params: joi.any(),
     result: joi.any()
+  },
+  'batch.edit': {
+    auth: false,
+    description: 'Bulk batch add',
+    notes: 'Bulk batch add',
+    params: joi.any(),
+    result: joi.any()
+  },
+  'batch.fetch': {
+    auth: false,
+    description: 'Bulk batch add',
+    notes: 'Bulk batch add',
+    params: joi.any(),
+    result: joi.any()
+  },
+  'batch.get': {
+    auth: false,
+    description: 'Bulk batch add',
+    notes: 'Bulk batch add',
+    params: joi.any(),
+    result: joi.any()
+  },
+  'payment.add': {
+    auth: false,
+    description: 'Bulk batch add',
+    notes: 'Bulk batch add',
+    params: joi.any(),
+    result: joi.any()
+  },
+  'payment.edit': {
+    auth: false,
+    description: 'Bulk batch add',
+    notes: 'Bulk batch add',
+    params: joi.any(),
+    result: joi.any()
   }
 }
