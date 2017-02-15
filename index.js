324bdc316d93622a0757c5e4a550eb5b76dac624
stamen.peev <stamen.peev@softwaregroup-bg.com>
2017-02-15 13:35:14 +0200

    fix: prepare cron

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-module.exports = require('ut-run').run({}, module)
+module.exports = require('ut-run')
+.run({}, module)
+.then(function (app) {
+  return app;
+  (function pay () {
+    app.bus.importMethod('bulk.x')()
+      .then(function (payments) {
+        var promise = Promise.resolve()
+        payments.forEach(function (payment) {
+          promise = promise.then(function () {
+            return app.bus.importMethod('bulk.y')(payment).catch(function () {})
+          })
+        })
+        return promise
+      })
+      .then(function () {
+        setTimeout(function () {
+          pay()
+        }, 10000)
+      })
+  })()
+})
--- service/bulk/index.js
+++ service/bulk/index.js
@@ -1,9 +1,23 @@
 // var error = require('./error')
 var path = require('path')
-
 module.exports = {
   schema: [{
     path: path.join(__dirname, 'schema'),
     linkSP: true
-  }]
+  }],
+  'x': function (msg) {
+    return [{
+      paymentId: '1',
+      queueId: 1
+    }, {
+      paymentId: '2',
+      queueId: 1
+    }, {
+      paymentId: '3',
+      queueId: 1
+    }]
+  },
+  'y': function (msg) {
+    return msg
+  }
 }
