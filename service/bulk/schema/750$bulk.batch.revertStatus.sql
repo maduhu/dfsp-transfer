8b1df5b0a7b70c47d0356b0d5624c2ba79a08bf2
Radoslav Viktorov <Radoslav.viktorov@Softwaregroup-bg.com>
2017-02-15 18:07:01 +0200

    fix: add revert batch status

---
+++ service/bulk/schema/750$bulk.batch.revertStatus.sql
@@ -0,0 +1,47 @@
+CREATE OR REPLACE FUNCTION bulk."batch.revertStatus" (
+    "@batchId" INTEGER,
+    "@actorId" VARCHAR(25)
+)
+RETURNS TABLE (
+    "batchId" integer,
+    "accountNumber" varchar(25),
+    "expirationDate" timestamp,
+    "name" varchar(100),
+    "batchStatusId" smallint,
+    "batchInfo" text,
+    "uploadInfo" text,
+    "actorId" varchar(25),
+    "fileName" varchar(256),
+    "originalFileName" varchar(256)
+) AS
+$body$
+BEGIN
+   RETURN QUERY
+    SELECT * 
+    FROM 
+        bulk."batch.edit"(
+            "@batchId",
+            NULL,
+            NULL,
+            NULL,
+            (
+                SELECT 
+                    bh."batchStatusId"
+                FROM
+                    bulk."batchHistory" bh 
+                WHERE 
+                    bh."batchId" = "@batchId" 
+                ORDER BY 
+                    bh."batchHistoryId" DESC
+                LIMIT 
+                    1
+            ),
+            NULL,
+            NULL,
+            "@actorId",
+            NULL,
+            NULL
+        );
+END;
+$body$
+LANGUAGE 'plpgsql';
