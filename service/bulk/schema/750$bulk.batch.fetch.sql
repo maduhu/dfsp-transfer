c1b215d601efdd9a8e6a032b014afe3a9178f21f
Ventsislav Dimitrov <ventsislav.dimitrov@softwaregroup-bg.com>
2017-02-20 19:18:54 +0200

    fix: last validated on date on batch.fetch

--- service/bulk/schema/750$bulk.batch.fetch.sql
+++ service/bulk/schema/750$bulk.batch.fetch.sql
@@ -41,26 +41,45 @@ BEGIN
         -- u."originalFileName",
         b."createdAt",
         (
-            SELECT MAX(x."date")
+            SELECT MAX(x."createdAt")
             FROM (
+                WITH orderedhistory AS
                 (
-                    SELECT bh."createdAt" as "date"
-                    FROM bulk."batchHistory" bh
-                    WHERE bh."batchId" = b."batchId" AND bh."batchStatusId" = ANY("@validationStatuses")
+                    SELECT
+                        row_number() over(ORDER BY bulk."batchHistory"."createdAt") AS "RowNum",
+                        bulk."batchHistory".*
+                    FROM bulk."batchHistory"
+                    WHERE bulk."batchHistory"."batchId" = b."batchId"
+                )
+                (
+                    SELECT
+                        orderedhistory1."batchStatusId",
+                        orderedhistory2."createdAt"
+                    FROM orderedhistory orderedhistory1
+                    JOIN orderedhistory orderedhistory2
+                        ON orderedhistory1."RowNum" = orderedhistory2."RowNum" + 1
                 )
                 UNION
                 (
-                    SELECT bb."createdAt" as "date"
-                    FROM bulk."batch" AS bb
-                    WHERE bb."batchId" = b."batchId" AND bb."batchStatusId" = ANY("@validationStatuses")
+                    SELECT
+                        bb."batchStatusId",
+                        (
+                            SELECT max(bh."createdAt")
+                            FROM bulk."batchHistory" bh
+                            WHERE bh."batchId" = bb."batchId" AND bb."batchStatusId" <> bh."batchStatusId"
+                        ) AS "createdAt"
+                    FROM bulk."batch" bb
+                    WHERE bb."batchId" = b."batchId"
                 )
+                ORDER BY "createdAt"
             ) AS x
+            WHERE x."batchStatusId" = ANY("@validationStatuses")
         ) AS "lastValidation",
         (
             SELECT COUNT(p."paymentId")
             FROM bulk."payment" p
             WHERE p."batchId" = b."batchId"
-        ) as "paymentsCount"
+        ) AS "paymentsCount"
     FROM 
         bulk."batch" AS b
     JOIN
