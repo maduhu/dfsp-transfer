7f9a1a80fe32351c57aae9cb54ca063016d6061e
stamen.peev <stamen.peev@softwaregroup-bg.com>
2017-02-23 19:32:56 +0200

    feat: remove scheduler and add methods 'bulk.batch.process', 'bulk.payment.getForProcessing', 'bulk.payment.preProcess' and 'bulk.payment.process'

--- service/bulk/index.js
+++ service/bulk/index.js
@@ -5,19 +5,69 @@ module.exports = {
     path: path.join(__dirname, 'schema'),
     linkSP: true
   }],
-  'payment.getForProcessing': function (msg) {
-    return [{
-      paymentId: '1',
-      queueId: 1
-    }, {
-      paymentId: '2',
-      queueId: 1
-    }, {
-      paymentId: '3',
-      queueId: 1
-    }]
+  'batch.process': function (msg, $meta) {
+    var count = 0
+    var pageNumber = 1
+    var get = () => {
+      return this.bus.importMethod('bulk.payment.fetch')({
+        pageSize: 100,
+        batchId: msg.batchId,
+        actorId: msg.actorId,
+        pageNumber: pageNumber++
+      })
+      .then((payments) => {
+        if (!payments.length) {
+          return count
+        }
+        return this.bus.importMethod('queue.queue.add')({
+          actorId: msg.actorId,
+          recordId: payments.map(payment => payment.paymentId)
+        })
+        .then((inserted) => {
+          count += inserted
+          return get()
+        })
+      })
+    }
+    return new Promise((resolve, reject) => {
+      return get()
+      .then(count => {
+        return resolve(count)
+      })
+    })
   },
-  'payment.process': function (msg) {
-    return msg
+  'payment.getForProcessing': function (msg) {
+    return this.bus.importMethod('queue.queue.fetch')({
+      count: 100
+    })
+  },
+  // actorId, paymentId
+  'payment.preProcess': function (msg) {
+    return this.bus.importMethod('bulk.payment.fetch')({
+      paymentId: [msg.paymentId]
+    })
+    .then((payment) => {
+      return this.bus.importMethod('queue.queue.update')({
+        recordId: payment[0].paymentId
+      })
+      .then(() => payment)
+    })
+  },
+  'payment.process': function (msg) { // actorId, paymentId, paymentStatusId, error
+    return this.bus.importMethod('bulk.payment.edit')({
+      actorId: msg.actorId,
+      payments: [{
+        paymentId: msg.paymentId,
+        paymentStatusId: msg.paymentStatusId,
+        info: msg.error
+      }]
+    })
+    .then(() => {
+      if (!msg.error) {
+        return this.bus.importMethod('queue.queue.remove')({
+          recordId: msg.paymentId
+        })
+      }
+    })
   }
 }
