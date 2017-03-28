34e43ca99b46bf6d6b82fe40b0a07c869fc85f91
Radoslav Viktorov <Radoslav.viktorov@Softwaregroup-bg.com>
2017-03-28 19:00:37 +0300

    fix: add integration tests for bulk.batch.get/fetch  - add additional check for missing batchId in batch.get

---
+++ test/integration/bulk/test.bulk.batch.fetch.js
@@ -0,0 +1,155 @@
+var test = require('ut-run/test')
+var config = require('../../lib/appConfig')
+const TIMESTAMP = (new Date()).getTime()
+const NAME = 'Batch-' + TIMESTAMP
+const ACTOR_ID = '' + Math.floor(Math.random() * 10)
+const FILE_NAME = 'File-' + TIMESTAMP
+const ORIGINAL_FILE_NAME = 'OriginalFile-' + TIMESTAMP
+
+test({
+  type: 'integration',
+  name: 'DFSP bulk fetch test',
+  server: config.server,
+  serverConfig: config.serverConfig,
+  client: config.client,
+  clientConfig: config.clientConfig,
+  steps: function (test, bus, run) {
+    run(test, bus, [{
+      name: 'Add new batch and check returned result types',
+      method: 'bulk.batch.add',
+      params: (context) => {
+        return {
+          name: NAME,
+          actorId: ACTOR_ID,
+          fileName: FILE_NAME,
+          originalFileName: ORIGINAL_FILE_NAME
+        }
+      },
+      result: (result, assert) => {
+        assert.true((typeof result.actorId === 'string'), 'Check actorId result type')
+        assert.true((typeof result.batchId === 'number'), 'Check batchId result type')
+        assert.true((typeof result.name === 'string'), 'Check name result type')
+        assert.true((typeof result.batchStatusId === 'number'), 'Check batchStatusId result type')
+      }
+    },
+    {
+      name: 'Fetch batch by actorId',
+      method: 'bulk.batch.fetch',
+      params: (context) => {
+        return {
+          actorId: ACTOR_ID
+        }
+      },
+      result: function (result, assert) {
+        assert.true(Array.isArray(result), 'Check if the returned result is array')
+        assert.true(result.length > 0, 'Check if the returned result is not empty')
+      }
+    },
+    {
+      name: 'Fetch batch by name',
+      method: 'bulk.batch.fetch',
+      params: (context) => {
+        return {
+          name: NAME
+        }
+      },
+      result: (result, assert) => {
+        assert.true(Array.isArray(result), 'Check if the returned result is array - original name')
+        assert.true(result.length > 0, 'Check if the returned result is not empty - original name')
+      }
+    },
+    {
+      name: 'Fetch batch by name - case insensitive - name to uppercase',
+      method: 'bulk.batch.fetch',
+      params: (context) => {
+        return {
+          name: NAME.toUpperCase()
+        }
+      },
+      result: (result, assert) => {
+        assert.true(Array.isArray(result), 'Check if the returned result is array - name to uppercase')
+        assert.true(result.length > 0, 'Check if the returned result is not empty - name to uppercase')
+      }
+    },
+    {
+      name: 'Fetch batch by name - case insensitive - name to lowercase',
+      method: 'bulk.batch.fetch',
+      params: (context) => {
+        return {
+          name: NAME.toLowerCase()
+        }
+      },
+      result: function (result, assert) {
+        assert.true(Array.isArray(result), 'Check if the returned result is array - name to lowercase')
+        assert.true(result.length > 0, 'Check if the returned result is not empty - name to lowercase')
+      }
+    },
+    {
+      name: 'Fetch batch by inserted batch properties - name',
+      method: 'bulk.batch.fetch',
+      params: (context) => {
+        return {
+          name: context['Add new batch and check returned result types'].name
+        }
+      },
+      result: function (result, assert) {
+        assert.true(Array.isArray(result), 'Check if the returned result is array - inserted name')
+        assert.true(result.length > 0, 'Check if the returned result is not empty - inserted name')
+      }
+    },
+    {
+      name: 'Fetch batch by inserted batch properties - name',
+      method: 'bulk.batch.fetch',
+      params: (context) => {
+        return {
+          batchStatusId: context['Add new batch and check returned result types'].batchStatusId
+        }
+      },
+      result: function (result, assert) {
+        assert.true(Array.isArray(result), 'Check if the returned result is array - inserted batchStatusId')
+        assert.true(result.length > 0, 'Check if the returned result is not empty - inserted batchStatusId')
+      }
+    },
+    {
+      name: 'Fetch batch by fromDate',
+      method: 'bulk.batch.fetch',
+      params: (context) => {
+        return {
+          fromDate: (new Date().setDate((new Date().getDay() - 1))).getDay
+        }
+      },
+      result: function (result, assert) {
+        assert.true(Array.isArray(result), 'Check if the returned result is array - fromDate')
+        assert.true(result.length > 0, 'Check if the returned result is not empty - fromDate')
+      }
+    },
+    {
+      name: 'Fetch batch by toDate',
+      method: 'bulk.batch.fetch',
+      params: (context) => {
+        return {
+          toDate: (new Date()).getDay
+        }
+      },
+      result: function (result, assert) {
+        assert.true(Array.isArray(result), 'Check if the returned result is array - toDate')
+        assert.true(result.length > 0, 'Check if the returned result is not empty - toDate')
+      }
+    },
+    {
+      name: 'Fetch batch by (fromDate - toDate)',
+      method: 'bulk.batch.fetch',
+      params: (context) => {
+        return {
+          fromDate: (new Date().setDate((new Date().getDay() - 1))).getDay,
+          toDate: (new Date()).getDay
+        }
+      },
+      result: function (result, assert) {
+        assert.true(Array.isArray(result), 'Check if the returned result is array - (fromDate - toDate)')
+        assert.true(result.length > 0, 'Check if the returned result is not empty - (fromDate - toDate)')
+      }
+    }
+    ])
+  }
+}, module.parent)
---
+++ test/integration/bulk/test.bulk.batch.get.js
@@ -0,0 +1,75 @@
+var test = require('ut-run/test')
+var config = require('../../lib/appConfig')
+var utError = require('ut-error')
+const TIMESTAMP = (new Date()).getTime()
+const NAME = 'Batch-' + TIMESTAMP
+const ACTOR_ID = '' + Math.floor(Math.random() * 10)
+const FILE_NAME = 'File-' + TIMESTAMP
+const ORIGINAL_FILE_NAME = 'OriginalFile-' + TIMESTAMP
+const BATCH_STATUS_UPLOADING = 'uploading'
+const ZERO_PAYMENTS = '0'
+
+test({
+  type: 'integration',
+  name: 'DFSP bulk fetch test',
+  server: config.server,
+  serverConfig: config.serverConfig,
+  client: config.client,
+  clientConfig: config.clientConfig,
+  steps: function (test, bus, run) {
+    run(test, bus, [{
+      name: 'Add new batch - for bulk batch get',
+      method: 'bulk.batch.add',
+      params: (context) => {
+        return {
+          name: NAME,
+          actorId: ACTOR_ID,
+          fileName: FILE_NAME,
+          originalFileName: ORIGINAL_FILE_NAME
+        }
+      },
+      result: (result, assert) => {
+        assert.true((typeof result.actorId === 'string'), 'Check actorId result type')
+        assert.true((typeof result.batchId === 'number'), 'Check batchId result type')
+        assert.true((typeof result.name === 'string'), 'Check name result type')
+        assert.true((typeof result.batchStatusId === 'number'), 'Check batchStatusId result type')
+      }
+    },
+    {
+      name: 'Get batch by batchId',
+      method: 'bulk.batch.get',
+      params: (context) => {
+        return {
+          batchId: context['Add new batch - for bulk batch get'].batchId
+        }
+      },
+      result: function (result, assert) {
+        assert.true(result.account === null, 'Check if the returned account is empty')
+        assert.true(result.actorId === ACTOR_ID, 'Check if the returned actorId is stored corectly')
+        assert.true(result.batchId === this['Add new batch - for bulk batch get'].batchId, 'Check the returned batchId')
+        assert.true(result.batchStatusId === this['Add new batch - for bulk batch get'].batchStatusId, 'Check the returned batchStatusId')
+        assert.false(isNaN(Date.parse(result.createdAt)), 'Check if the returned result is a Date')
+        assert.true(result.expirationDate === null, 'Check that expirationDate is not set')
+        assert.true(result.fileName === FILE_NAME, 'Check the fileName value')
+        assert.true(result.info === '', 'Check that the info is empty')
+        assert.true(result.name === NAME, 'Check the batch name')
+        assert.true(result.originalFileName === ORIGINAL_FILE_NAME, 'Check the batch original fileName')
+        assert.true(result.paymentsCount === ZERO_PAYMENTS, 'Check that there arent any asociated with this batch payments')
+        assert.true(result.status === BATCH_STATUS_UPLOADING, 'Check the batch status')
+        assert.true(result.updatedAt === null, 'Check that the batch has not been updated')
+      }
+    },
+    {
+      name: 'Get batch - missing batchId',
+      method: 'bulk.batch.get',
+      params: (context) => {
+        return {}
+      },
+      error: function (error, assert) {
+        assert.true(error instanceof utError.get('bulk.batchIdMissing'), error.errorPrint)
+        assert.equal(error.errorPrint, 'Missing batchId', error.errorPrint)
+      }
+    }
+    ])
+  }
+}, module.parent)
