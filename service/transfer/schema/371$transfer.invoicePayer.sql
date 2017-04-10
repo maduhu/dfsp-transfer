354a99a745341a28bd39ec7c57ead584f3cff5fe
Ventsislav Dimitrov <ventsislav.dimitrov@softwaregroup-bg.com>
2017-04-10 11:38:31 +0300

    fix: add invoicePayer table

---
+++ service/transfer/schema/371$transfer.invoicePayer.sql
@@ -0,0 +1,8 @@
+CREATE TABLE transfer."invoicePayer" (
+    "invoicePayerId" BIGSERIAL,
+    "invoiceId" INTEGER,
+    "identifier" VARCHAR(25),
+    "createdAt" TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    CONSTRAINT "pkInvoicePayerId" PRIMARY KEY ("invoicePayerId"),
+    CONSTRAINT "fkInvoicePayer_invoiceId" FOREIGN KEY ("invoiceId") REFERENCES transfer."invoice"("invoiceId")
+)
