17ddabe96ac3e7780ebdc64804d1dc5cffdfdd73
Radoslav Viktorov <Radoslav.viktorov@Softwaregroup-bg.com>
2017-07-13 18:46:36 +0300

    fix: add user data obfuscation config

--- httpserver/index.js
+++ httpserver/index.js
@@ -3,6 +3,16 @@ module.exports = {
   id: 'httpserver',
   createPort: require('ut-port-httpserver'),
   logLevel: 'trace',
+  log: {
+    transform: {
+      payee: 'hide',
+      name: 'hide',
+      firstName: 'hide',
+      lastName: 'hide',
+      nationalId: 'hide',
+      dob: 'hide'
+    }
+  },
   api: ['transfer', 'bulk'],
   imports: ['transfer.start'],
   port: 8018,
