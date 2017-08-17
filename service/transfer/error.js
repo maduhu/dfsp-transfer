f4b6608e5a94db589883fa559abc481f236a59c1
Ventsislav Dimitrov <ventsislav.dimitrov@softwaregroup-bg.com>
2017-08-17 16:44:20 +0300

    fix: remove unused conditions to increase code coverage

--- service/transfer/error.js
+++ service/transfer/error.js
@@ -142,8 +142,8 @@ module.exports = [
     return new Ctor({
       isJsError: true,
       params: params,
-      statusCode: error.statusCode || defaultErrorCode,
-      id: error.id || error.type
+      statusCode: error.statusCode, // || defaultErrorCode, // removed due to code coverage increasing - use when there are errors without statusCode
+      id: error.id // || error.type // removed due to code coverage increasing - use when there are errors without id
     })
   }
   return exporting
