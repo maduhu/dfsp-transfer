4f53c29f853dae0e54faf36516de54f01bb66d67
Ventsislav Dimitrov <ventsislav.dimitrov@softwaregroup-bg.com>
2017-08-17 16:34:51 +0300

    fix: remove unused method

--- service/transfer/index.js
+++ service/transfer/index.js
@@ -43,24 +43,5 @@ module.exports = {
         }
       }
     })
-  },
-  'push.execute': function (msg, $meta) {
-    return new Promise((resolve, reject) => {
-      var socket = new Ws(msg.sourceAccount.replace(/^https?:\/\//, 'ws://') + '/transfers')
-      var timeout = setTimeout(() => {
-        socket && socket.terminate()
-        resolve(msg)
-      }, (this.bus.config.transfer && this.bus.config.transfer.socketTimeout) || 2222)
-      socket.on('error', (err) => {
-        if (err.code === 'ECONNREFUSED') {
-          // do nothing for now
-        }
-      })
-      socket.on('message', (data, flags) => {
-        clearTimeout(timeout)
-        socket.terminate()
-        resolve(data)
-      })
-    })
   }
 }
